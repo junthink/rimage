f4c5b79a41bb2730d1d30f2d9ed6a22016c21167
YangJun <junthink@gmail.com>
2019-08-28 16:06:47 +0800

    fix

--- src/demo/ts/Demo.ts
+++ src/demo/ts/Demo.ts
@@ -5,9 +5,15 @@ declare let tinymce: any;
 
 Plugin();
 
+const service = axios.create({
+    baseURL: 'https://api1.rongcat.com',
+    timeout: 90000,
+    headers: { Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGkxLnJvbmdjYXQuY29tXC9hdXRoXC9hZG1pbiIsImlhdCI6MTU2NjU0NzkyNiwiZXhwIjoxNTY2OTc5OTI2LCJuYmYiOjE1NjY1NDc5MjYsImp0aSI6IkVDblZvaTRsUHZiR2NmeFAiLCJzdWIiOjI0LCJwcnYiOiJjNDFkNmY0ZjZlMmExOWUyOTRiYWQ1YWI3OGU2YTY3Mzg0NjdmNWJkIn0.r1f4vfuGVttRab5rS4xXO4HMeriIZduOwE3sDeEG-ak' }
+});
+
 tinymce.init({
     selector: 'textarea.tinymce',
     plugins: 'code rimage',
     toolbar: 'rimage',
-    request: axios
+    request: service
 });
--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -9,24 +9,33 @@ const setup = (editor, url) => {
             const request = editor.getParam('request');
             const uploader = new Uploader();
             uploader.selectFile().then(function (files) {
+                let content = '';
+                let images = {};
+                for (let i = 0; i < files.length; i++) {
+                    const imageId = `image-${Math.round(Math.random() * 10000000)}`;
+                    content += `<p id="${imageId}-box" style="margin: 0; padding: 0;"></p>`;
+                    images[i] = imageId;
+                }
+                editor.insertContent(content);
                 for (let i = 0; i < files.length; i++) {
                     const file = files.item(i);
                     uploader.convertFileToBase64(file).then(function (v) {
-                        const imageId = `image-${Math.round(Math.random() * 100000000)}`;
-                        editor.insertContent(`<p style="text-align: center;"><img id="${imageId}" src="${v}" /></p>`);
+                        let imageBox = editor.dom.get(images[i] + '-box');
+                        imageBox.innerHTML = `<img id="${images[i]}" src="${v}" style="margin: 0; padding: 0;" />`;
                         const form = new FormData();
                         form.append('upload_file', file);
+                        form.append('upload_custom', JSON.stringify({watermark: 'default'}));
                         request({
                             url: '/upload',
                             method: 'post',
                             data: form
                         }).then((resData) => {
-                            console.log('==============');
-                            const image =  editor.dom.get(imageId);
-                            image.src = resData.data.image_url;
-                            image.setAttrib('author','rongcat');
+                            let newImageBox = editor.dom.get(images[i] + '-box');
+                            let image = editor.dom.get(images[i]);
+                            editor.dom.remove(image);
+                            let src = resData.data.image_url;
+                            newImageBox.innerHTML = `<img id="${images[i]}" src="${src}" style="margin: 0; padding: 0;" />`;
                         }).catch((err) => {
-                            console.log(err);
                         });
                     });
                 }
@@ -38,4 +47,5 @@ const setup = (editor, url) => {
 tinymce.PluginManager.add('rimage', setup);
 
 // tslint:disable-next-line:no-empty
-export default () => {};
+export default () => {
+};
